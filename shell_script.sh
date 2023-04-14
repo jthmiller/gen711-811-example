11f840000143ca8313201a718cf3acc6a8d9c0ec
jtmiller <jtmiller@umn.edu>
2023-04-14 14:49:39 -0400

    more

--- shell_script.sh
+++ shell_script.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
 
+## Run fastqc on one of the samples
+cd /mydir/fastqs
+fastqc SRR000001.fastq
 
+## Move the html file to the results folder
+mv SRR000001_fastqc.html ../results
 
 ## This performs trimming
-trimmomatic.pl pde1.fastg
+cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -o SRR000001_trimmed.fastq SRR000001.fastq
  
 ## This command gets per base cov 
-bedtools
+qiime import --type 'SampleData[PairedEndSequencesWithQuality]' --input-path /mydir/fastqs --input-format CasavaOneEightSingleLanePerSampleDirFmt --output-path /mydir/seqs.qza
