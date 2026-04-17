c31cdecf8a63f1f730c2665463697d10c3efa3b0
jthmiller <jeffrey.miller@unh.edu>
2026-04-17 14:28:19 -0400

    sdf

--- code/inti-repo.sh
+++ code/inti-repo.sh
@@ -1,4 +1,18 @@
 mkdir code/ 
 mkdir plots/ 
 mkdir results/
+mkdir metadata/
 mkdir -p data/fastqs
+mkdir -p data/poly-G-trimmed
+mkdir -p data/html
+
+
+## if you made the poly-G-trimmed directory in your working directory, you can move it to the data directory. 
+mv poly-G-trimmed/ data/poly-G-trimmed/
+## make sure the poly-G-trimmed directory is in the .gitignore file, so that the large files are not tracked by git
+
+## Copy the fastq files to the data/fastqs directory
+## Before doing this, make sure the fastqs directory is on the .gitignore file, so that the large files are not tracked by git
+cp /tmp/GEN711-811_data/MassDEP/fastqs/*.fastq.gz data/fastqs/
+
+## Proceed to 01_trim.sh
--- code/polyGfilter.sh
+++ code/polyGfilter.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo -e "begin polyG filter...\n"
-echo "polyG filter length threshold: ${1}\n"
-echo "polyG filter will remove polyG tails and also filter out reads that are too short after trimming.\n"
+echo  -e "polyG filter length threshold: ${1}\n"
+echo  -e "polyG filter will remove polyG tails and also filter out reads that are too short after trimming.\n"
 
- for f in *R1_001.fastq.gz ; do 
- 
+ for fq in data/fastqs/*R1_001.fastq.gz ; do 
+    f=$(basename $fq)
     r=$(echo $f | sed 's/_R1_/_R2_/g')
      
      fastp \
-     --in1 $f \
-     --in2 $r \
-     --html html/${f%_L002_R1_001.fastq.gz}.html \
-     --out1 poly-G-trimmed/$f \
-     --out2 poly-G-trimmed/$r \
+     --in1 data/fastqs/$f \
+     --in2 data/fastqs/$r \
+     --html data/html/${f%_L002_R1_001.fastq.gz}.html \
+     --out1 data/poly-G-trimmed/$f \
+     --out2 data/poly-G-trimmed/$r \
      --cut_tail \
      --poly_g_min_len 4 \
      --cut_tail_mean_quality 25 \
@@ -27,10 +27,10 @@ echo "polyG filter will remove polyG tails and also filter out reads that are to
  done > fastp.out 2>&1
  
 
-echo "info in fastp.out\n"
-echo -e "\npolyG filter complete.\n"
-echo "counting reads before and after polyG filter...\n"
+echo  -e "info in fastp.out\n"
+echo  -e "\npolyG filter complete.\n"
+echo  -e "counting reads before and after polyG filter...\n"
 
-for fq in *R1_001.fastq.gz ; do 
-    echo "$(basename $fq | sed 's/_L002_R1_001.fastq.gz//' ) $(zcat $fq | grep '^@' | wc -l) $(zcat poly-G-trimmed/$fq | grep '^@' | wc -l)" 
-done | sort -k2 -h | awk -v OFS='\t' '{ print $1,$2,$3 }'
+for fq in data/fastqs/*R1_001.fastq.gz ; do 
+    echo  -e  "$(basename $fq | sed 's/_L002_R1_001.fastq.gz//' ) $(zcat $fq | grep '^@' | wc -l) $(zcat data/poly-G-trimmed/$fq | grep '^@' | wc -l)" 
+done | sort -k2 -h | awk -v OFS='\t' '{ print $1,$2,$3 }' > polyG_filter_read_counts.tsv
